3680e4c0604fc68b7c55c1c20f51910f3a53d191
Pablo <gmlpubli00@gmail.com>
2022-04-24 18:29:52 -0300

    avance cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,16 @@
-const Card = () => {
-	return <div></div>
+import logo from "../assets/images/inco-logo.png"
+const Card = ({ heading }) => {
+  return (
+    <div className="card__ctn">
+      <div className="card__img-ctn">
+        <img src={logo} alt="logo" className="card__img" />
+      </div>
+      <div className="card__text-ctn">
+        <h3 className="card__title">Inco</h3>
+        <p className="card__text"></p>
+      </div>
+    </div>
+  )
 }
 
 export default Card
--- src/components/CardCallery.jsx
+++ src/components/CardCallery.jsx
@@ -3,43 +3,49 @@ import Card from "./Card"
 import { SearchIcon } from "./Icons"
 
 const Cardsgallery = ({ cards = [], path = ["hola", "como", "estas"] }) => {
-	const handleInputChange = (e) => {
-		console.log(e.target.value)
-		return e.target.value
-	}
-	return (
-		<section className="card-gallery__ctn">
-			<div className="card-gallery__top">
-				<div className="card-gallery__links-ctn">
-					<ul>
-						<li>
-							<Link to="/">Instituto:</Link>
-						</li>
-						{path.map((item, index, array) => (
-							<li key={index + item}>
-								<Link to={`/${array.slice(0, index + 1).join("/")}`}>
-									{item}
-								</Link>
-							</li>
-						))}
-					</ul>
-				</div>
-				<div className="card-gallery__search-ctn">
-					<input
-						type="text"
-						placeholder="Buscar..."
-						className="card-gallery__search"
-						onChange={handleInputChange}
-					/>
-					<SearchIcon size="20" color="#333" />
-				</div>
-			</div>
-			<div className="card-gallery__ctn">
-				{cards.map((card) => (
-					<Card key={card.id} card={card} path={path} />
-				))}
-			</div>
-		</section>
-	)
+  const handleInputChange = (e) => {
+    console.log(e.target.value)
+    return e.target.value
+  }
+  return (
+    <section className="card-gallery__ctn">
+      <div className="card-gallery__top">
+        <div className="card-gallery__links-ctn">
+          <ul>
+            <li>
+              <Link to="/">Instituto:</Link>
+            </li>
+            {path.map((item, index, array) => (
+              <li key={index + item}>
+                <Link to={`/${array.slice(0, index + 1).join("/")}`}>
+                  {item}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="card-gallery__search-ctn">
+          <input
+            type="text"
+            placeholder="Buscar..."
+            className="card-gallery__search"
+            onChange={handleInputChange}
+          />
+          <SearchIcon size="20" color="#333" />
+        </div>
+      </div>
+      <div className="card-gallery__cards-ctn">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        {cards.map((card) => (
+          <Card key={card.id} card={card} path={path} />
+        ))}
+      </div>
+    </section>
+  )
 }
 export default Cardsgallery
