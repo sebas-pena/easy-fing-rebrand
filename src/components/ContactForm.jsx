caec7cb0428c9078c678f3ea99a8fb41140e7cb8
Pablo <gmlpubli00@gmail.com>
2022-04-27 15:10:03 -0300

    avance form

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,35 +1,51 @@
 import { useState, useEffect, useRef } from "react"
+import { UploadIcon } from "./Icons"
 
 const ContactForm = () => {
-	const [disableSubmit, setDisableSubmit] = useState(true)
-	return (
-		<div className="contact-form__ctn">
-			<form className="contact-form">
-				<div className="contact-form__input-ctn">
-					<label htmlFor="name">Nombre</label>
-					<input id="name" type="text" />
-				</div>
-				<div className="contact-form__input-ctn">
-					<label htmlFor="email">Email</label>
-					<input id="email" type="text" />
-				</div>
-				<div className="contact-form__input-ctn">
-					<label htmlFor="message">Mensaje</label>
-					<textarea id="message" />
-				</div>
-				<div className="contact-form__file-input-ctn">
-					<label htmlFor="file">Adjuntar archivo</label>
-					<input id="file" type="file" />
-				</div>
-				<input
-					className="contact-form__submit"
-					type="submit"
-					value="Enviar"
-					{...(disableSubmit ? { disabled: true } : {})}
-				/>
-			</form>
-		</div>
-	)
+  const [disableSubmit, setDisableSubmit] = useState(false)
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(e)
+  }
+
+  const handleChange = ({ target }) => {
+    console.log(target)
+    if (target.value.length > 0) {
+    }
+  }
+
+  return (
+    <div className="contact-form__ctn">
+      <form className="contact-form" onSubmit={handleSubmit}>
+        <div className="contact-form__input-ctn">
+          <label htmlFor="name">Nombre</label>
+          <input id="name" type="text" onChange={handleChange} />
+        </div>
+        <div className="contact-form__input-ctn">
+          <label htmlFor="email">Email</label>
+          <input id="email" type="text" onChange={handleChange} />
+        </div>
+        <div className="contact-form__input-ctn">
+          <label htmlFor="message">Mensaje</label>
+          <textarea id="message" onChange={handleChange} />
+        </div>
+        <div className="contact-form__bottom">
+          <label className="contact-form__file-input" htmlFor="file">
+            Adjuntar archivo
+            <UploadIcon size={13} color="#222" />
+          </label>
+          <input id="file" type="file" />
+          <input
+            className="contact-form__submit"
+            type="submit"
+            value="Enviar"
+            {...(disableSubmit ? { disabled: true } : {})}
+          />
+        </div>
+      </form>
+    </div>
+  )
 }
 
 export default ContactForm
