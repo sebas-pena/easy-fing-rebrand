a9db6e798aa2dc53d7264d5914ef5998b69b5567
Pablo Peña <insomnionebulous@gmail.com>
2022-04-28 07:19:20 -0300

    avance formulario contacto

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,19 +1,21 @@
 import { useState } from "react"
 
 const useForm = (initialState) => {
-  const [values, setValues] = useState(initialState)
+	const [values, setValues] = useState(initialState)
 
-  const handleInputChange = ({ target }) => {
-    setValues((values) => ({
-      ...values,
-      [target.name]: target.value,
-    }))
-  }
+	const handleInputChange = (e) => {
+		const { id, value, type, files } = e.target
 
-  return {
-    handleInputChange,
-    values,
-  }
+		setValues((values) => ({
+			...values,
+			[id]: type === "file" ? files[0] : value,
+		}))
+	}
+
+	return {
+		handleInputChange,
+		values,
+	}
 }
 
 export default useForm
